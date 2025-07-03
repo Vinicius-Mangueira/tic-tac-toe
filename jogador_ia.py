a41dd8dba2f7b9085b4a2e484e42f92a18653408
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2025-07-03 20:54:46 -0300

    .

--- jogador_ia.py
+++ jogador_ia.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""Atividade: implementar um sistema especialista que jogue o Jogo da
+Velha. Implemente as regras abaixo para o jogador inteligente. Estas regras
+devem ser testadas na ordem apresentada, se uma não levar a uma jogada
+passe para a próxima"""
 from random import randint
 
 from jogador import Jogador
@@ -15,79 +19,72 @@ class JogadorIA(Jogador):
             for c in range(0,3):
                 if self.matriz[l][c] == Tabuleiro.DESCONHECIDO:
                     lista.append((l, c))
-                # Se você ou seu oponente tiver duas marcações em sequência, marque o quadrado restante. 
-                elif self.matriz[l][c] == Tabuleiro.JOGADOR_0 and self.tipo == Tabuleiro.JOGADOR_X:
-                    if c < 2 and self.matriz[l][c+1] == Tabuleiro.JOGADOR_0 and self.matriz[l][c+2] == Tabuleiro.DESCONHECIDO:
-                        return (l, c+2)
-                    elif c > 0 and c < 2 and self.matriz[l][c-1] == Tabuleiro.JOGADOR_0 and self.matriz[l][c+1] == Tabuleiro.DESCONHECIDO:
-                        return (l, c+1)
-                    elif c > 1 and self.matriz[l][c-1] == Tabuleiro.JOGADOR_0 and self.matriz[l][c-2] == Tabuleiro.DESCONHECIDO:
-                        return (l, c-2)
-                    elif l < 2 and self.matriz[l+1][c] == Tabuleiro.JOGADOR_0 and self.matriz[l+2][c] == Tabuleiro.DESCONHECIDO:
-                        return (l+2, c)
-                    elif l > 0 and l < 2 and self.matriz[l-1][c] == Tabuleiro.JOGADOR_0 and self.matriz[l+1][c] == Tabuleiro.DESCONHECIDO:
-                        return (l+1, c)
-                    elif l > 1 and self.matriz[l-1][c] == Tabuleiro.JOGADOR_0 and self.matriz[l-2][c] == Tabuleiro.DESCONHECIDO:
-                        return (l-2, c)
-                    elif l < 2 and c < 2 and self.matriz[l+1][c+1] == Tabuleiro.JOGADOR_0 and self.matriz[l+2][c+2] == Tabuleiro.DESCONHECIDO:
-                        return (l+2, c+2)
-                    elif l > 0 and l < 2 and c > 0 and c < 2 and self.matriz[l-1][c-1] == Tabuleiro.JOGADOR_0 and self.matriz[l+1][c+1] == Tabuleiro.DESCONHECIDO:
-                        return (l+1, c+1)
-                    elif l > 1 and c > 1 and self.matriz[l-1][c-1] == Tabuleiro.JOGADOR_0 and self.matriz[l-2][c-2] == Tabuleiro.DESCONHECIDO:
-                        return (l-2, c-2)
-                    elif l < 2 and c > 0 and self.matriz[l+1][c-1] == Tabuleiro.JOGADOR_0 and self.matriz[l+2][c-2] == Tabuleiro.DESCONHECIDO:
-                        return (l+2, c-2)
-                    elif l > 0 and l < 2 and c > 0 and c < 2 and self.matriz[l-1][c+1] == Tabuleiro.JOGADOR_0 and self.matriz[l+1][c-1] == Tabuleiro.DESCONHECIDO:
-                        return (l+1, c-1)
-                    elif l > 1 and c < 2 and self.matriz[l-1][c+1] == Tabuleiro.JOGADOR_0 and self.matriz[l-2][c-2] == Tabuleiro.DESCONHECIDO:
-                        return (l-2, c-2)
-                #Se houver uma jogada que crie duas sequências de duas marcações,use-a.
-                elif self.matriz[l][c] == Tabuleiro.JOGADOR_X and self.tipo == Tabuleiro.JOGADOR_0:
-                    if c < 2 and self.matriz[l][c+1] == Tabuleiro.JOGADOR_X and self.matriz[l][c+2] == Tabuleiro.DESCONHECIDO:
-                        return (l, c+2)
-                    elif c > 0 and c < 2 and self.matriz[l][c-1] == Tabuleiro.JOGADOR_X and self.matriz[l][c+1] == Tabuleiro.DESCONHECIDO:
-                        return (l, c+1)
-                    elif c > 1 and self.matriz[l][c-1] == Tabuleiro.JOGADOR_X and self.matriz[l][c-2] == Tabuleiro.DESCONHECIDO:
-                        return (l, c-2)
-                    elif l < 2 and self.matriz[l+1][c] == Tabuleiro.JOGADOR_X and self.matriz[l+2][c] == Tabuleiro.DESCONHECIDO:
-                        return (l+2, c)
-                    elif l > 0 and l < 2 and self.matriz[l-1][c] == Tabuleiro.JOGADOR_X and self.matriz[l+1][c] == Tabuleiro.DESCONHECIDO:
-                        return (l+1, c)
-                    elif l > 1 and self.matriz[l-1][c] == Tabuleiro.JOGADOR_X and self.matriz[l-2][c] == Tabuleiro.DESCONHECIDO:
-                        return (l-2, c)
-                    elif l < 2 and c < 2 and self.matriz[l+1][c+1] == Tabuleiro.JOGADOR_X and self.matriz[l+2][c+2] == Tabuleiro.DESCONHECIDO:
-                        return (l+2, c+2)
-                    elif l > 0 and l < 2 and c > 0 and c < 2 and self.matriz[l-1][c-1] == Tabuleiro.JOGADOR_X and self.matriz[l+1][c+1] == Tabuleiro.DESCONHECIDO:
-                        return (l+1, c+1)
-                    elif l > 1 and c > 1 and self.matriz[l-1][c-1] == Tabuleiro.JOGADOR_X and self.matriz[l-2][c-2] == Tabuleiro.DESCONHECIDO:
-                        return (l-2, c-2)
-                    elif l < 2 and c > 0 and self.matriz[l+1][c-1] == Tabuleiro.JOGADOR_X and self.matriz[l+2][c-2] == Tabuleiro.DESCONHECIDO:
-                        return (l+2, c-2)
-                    elif l > 0 and l < 2 and c > 0 and c < 2 and self.matriz[l-1][c+1] == Tabuleiro.JOGADOR_X and self.matriz[l+1][c-1] == Tabuleiro.DESCONHECIDO:
-                        return (l+1, c-1)
-                    elif l > 1 and c < 2 and self.matriz[l-1][c+1] == Tabuleiro.JOGADOR_X and self.matriz[l-2][c-2] == Tabuleiro.DESCONHECIDO:
-                        return (l-2, c-2)
-                    # R3. Se o quadrado central estiver livre, marque-o.
-                    if self.matriz[1][1] == Tabuleiro.DESCONHECIDO:
-                        return (1, 1)
-                    # R4. Se o oponente marcou um canto, marque o canto oposto.
-                    elif self.matriz[0][0] == Tabuleiro.JOGADOR_0 and self.matriz[2][2] == Tabuleiro.DESCONHECIDO:
-                        return (2, 2)
-                    elif self.matriz[0][2] == Tabuleiro.JOGADOR_0 and self.matriz[2][0] == Tabuleiro.DESCONHECIDO:
-                        return (2, 0)
-                    elif self.matriz[2][0] == Tabuleiro.JOGADOR_0 and self.matriz[0][2] == Tabuleiro.DESCONHECIDO:
-                        return (0, 2)
-                    elif self.matriz[2][2] == Tabuleiro.JOGADOR_0 and self.matriz[0][0] == Tabuleiro.DESCONHECIDO:
-                        return (0, 0)
-                    # R5. If there is an empty corner, mark it.
-                    elif self.matriz[0][0] == Tabuleiro.DESCONHECIDO:
-                        return (0, 0)
-                    elif self.matriz[0][2] == Tabuleiro.DESCONHECIDO:
-                        return (0, 2)
-                    elif self.matriz[2][0] == Tabuleiro.DESCONHECIDO:
-                        return (2, 0)
-                    elif self.matriz[2][2] == Tabuleiro.DESCONHECIDO:
-                        return (2, 2)
+                
+        # R1. Se você ou seu oponente tiver duas marcações em sequência, marque o quadrado restante.
+        for tipo in [self.tipo, 3 - self.tipo]:  # self.tipo = IA, 3-self.tipo = adversário
+            # Checa linhas
+            for l in range(3):
+                linha = [self.matriz[l][c] for c in range(3)]
+                if linha.count(tipo) == 2 and linha.count(Tabuleiro.DESCONHECIDO) == 1:
+                    c = linha.index(Tabuleiro.DESCONHECIDO)
+                    return (l, c)
+            # Checa colunas
+            for c in range(3):
+                coluna = [self.matriz[l][c] for l in range(3)]
+                if coluna.count(tipo) == 2 and coluna.count(Tabuleiro.DESCONHECIDO) == 1:
+                    l = coluna.index(Tabuleiro.DESCONHECIDO)
+                    return (l, c)
+            # Checa diagonal principal
+            diagonal = [self.matriz[i][i] for i in range(3)]
+            if diagonal.count(tipo) == 2 and diagonal.count(Tabuleiro.DESCONHECIDO) == 1:
+                i = diagonal.index(Tabuleiro.DESCONHECIDO)
+                return (i, i)
+            # Checa diagonal secundária
+            diagonal = [self.matriz[i][2-i] for i in range(3)]
+            if diagonal.count(tipo) == 2 and diagonal.count(Tabuleiro.DESCONHECIDO) == 1:
+                i = diagonal.index(Tabuleiro.DESCONHECIDO)
+                return (i, 2-i)
+        #R2. Se houver uma jogada que crie duas sequências de duas marcações,use-a.
+        for l, c in lista:
+            # Simula a jogada
+            self.matriz[l][c] = self.tipo
+            sequencias = 0
+            # Checa linhas
+            for i in range(3):
+                linha = [self.matriz[i][j] for j in range(3)]
+                if linha.count(self.tipo) == 2 and linha.count(Tabuleiro.DESCONHECIDO) == 1:
+                    sequencias += 1
+            # Checa colunas
+            for j in range(3):
+                coluna = [self.matriz[i][j] for i in range(3)]
+                if coluna.count(self.tipo) == 2 and coluna.count(Tabuleiro.DESCONHECIDO) == 1:
+                    sequencias += 1
+            # Checa diagonal principal
+            diagonal = [self.matriz[i][i] for i in range(3)]
+            if diagonal.count(self.tipo) == 2 and diagonal.count(Tabuleiro.DESCONHECIDO) == 1:
+                sequencias += 1
+            # Checa diagonal secundária
+            diagonal = [self.matriz[i][2-i] for i in range(3)]
+            if diagonal.count(self.tipo) == 2 and diagonal.count(Tabuleiro.DESCONHECIDO) == 1:
+                sequencias += 1
+            # Restaura o estado original
+            self.matriz[l][c] = Tabuleiro.DESCONHECIDO
+            if sequencias >= 2:
+                return (l, c)
+        # R3. Se o centro estiver livre, jogue nele.
+        if self.matriz[1][1] == Tabuleiro.DESCONHECIDO:
+            return (1, 1)
+        #R4. Se seu oponente tiver marcado um dos cantos, marque o canto oposto
+        cantos = [(0,0), (0,2), (2,0), (2,2)]
+        for (l, c) in cantos:
+            if self.matriz[l][c] == 3 - self.tipo:  # adversário
+                l_oposto, c_oposto = 2 - l, 2 - c
+                if self.matriz[l_oposto][c_oposto] == Tabuleiro.DESCONHECIDO:
+                    return (l_oposto, c_oposto)
+        # R5. Se um canto estiver livre, jogue nele.
+        for (l, c) in cantos:
+            if self.matriz[l][c] == Tabuleiro.DESCONHECIDO:
+                return (l, c)
         if(len(lista) > 0):
             p = randint(0, len(lista)-1)
             return lista[p]
